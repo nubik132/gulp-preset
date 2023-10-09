e3fe855f7ae56a71c5c8054ba974adf711932a62
nubik132 <mister.nubik@list.ru>
2023-10-09 17:59:59 +0300

    feat: made to cleat-css

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,8 +1,11 @@
+import replace from "gulp-replace";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
 import browsersync from "browser-sync";
 
 export const plugins = {
-    // replace: replace,
-    // plumber: plumber,
-    // notify: notify,
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
     browsersync: browsersync,
 } 
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
 import { plugins } from "./gulp/config/plugins.js";
 
+
+
 global.app = {
     path: path,
     gulp: gulp,
